764063ea1710c97d923229a07f50c49ed67e1f65
dawei <tb6147877@hotmail.com>
2024-03-03 19:52:34 +0000

    part4

---
+++ C2_ImprovingDeepNeuralNetworks/Part4/mianpart4.py
@@ -0,0 +1,19 @@
+### v1.1
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+
+from opt_utils_v1a import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils_v1a import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+from copy import deepcopy
+from testCases import *
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
